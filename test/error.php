ae0a66a7a210a42117ae200363e4eab5ba824d11
dupesh-chicmic <dupesh.malhan@chicmic.co.in>
2024-10-22 22:22:38 +0530

    Delete test directory

--- test/error.php
+++
@@ -1,6 +0,0 @@
-<?php
-$uri = 'http://' . $_SERVER['HTTP_HOST'] . 'test/view.php';
-if($uri != 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']){
-
-    $_SESSION['flag']=[];
-}
--- test/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-include 'error.php';
-header('location:login.php');
